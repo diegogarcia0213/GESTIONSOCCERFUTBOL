a569ea61bc0dda385a0984e7c86854c53bf5259a
Diego Garcia <dgarciam8@ucentral.edu.co>
2024-11-06 17:31:59 -0500

    (nuevo): se creo correctamente el boton de eliminar jugador activo

--- src/main/java/co/ucentral/GestionFUTBOL/persistencia/repositorio/RegistroPagosRepositorio.java
+++ src/main/java/co/ucentral/GestionFUTBOL/persistencia/repositorio/RegistroPagosRepositorio.java
@@ -1,8 +1,9 @@
 package co.ucentral.GestionFUTBOL.persistencia.repositorio;
 
-
 import co.ucentral.GestionFUTBOL.persistencia.entidades.RegistroPagos;
+import co.ucentral.GestionFUTBOL.persistencia.entidades.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RegistroPagosRepositorio extends JpaRepository<RegistroPagos, Long> {
+    void deleteByUsuario(Usuario usuario); // Método para eliminar registro de pagos por usuario
 }
